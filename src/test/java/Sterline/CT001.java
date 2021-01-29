e89b2fe83508d558ba9e80e5f61c7dc7f8663f19
Sterline Elisee <74082109+sterline92@users.noreply.github.com>
2021-01-29 14:37:02 -0600

    Initial commit

---
+++ src/test/java/Sterline/CT001.java
@@ -0,0 +1,10 @@
+package Sterline;
+
+public class CT001 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Trying git Push/ Pull");
+        // initial commit
+    }
+}
